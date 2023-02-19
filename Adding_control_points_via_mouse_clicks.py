91ef2e53bc992998a9757a9c0cef67a0f385f07b
NaoIsMe <116496738+NaoIsMe@users.noreply.github.com>
2023-02-19 10:57:11 +0000

    Add files via upload
    Copy and paste to Python console in 3-D Slicer. Requires UI built around it for a more intuitive experience.

---
+++ Adding_control_points_via_mouse_clicks.py
@@ -0,0 +1,15 @@
+#Source: https://slicer.readthedocs.io/en/latest/developer_guide/script_repository.html
+# Instructions:
+# Open Slicer>Open Python Console
+
+w=slicer.qSlicerMarkupsPlaceWidget()
+w.setMRMLScene(slicer.mrmlScene)
+markupsNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLMarkupsCurveNode")
+w.setCurrentNode(slicer.mrmlScene.GetNodeByID(markupsNode.GetID()))
+# Hide all buttons and only show place button
+w.buttonsVisible=False
+w.placeButton().show()
+w.show()
+
+placeModePersistence = 1
+slicer.modules.markups.logic().StartPlaceMode(placeModePersistence)
